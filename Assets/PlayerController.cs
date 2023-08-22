09bcf1558418d84d214e5c9a5d458affa2243f10
Carl Gulliksson <gulliksson.carl@gmail.com>
2023-08-22 23:31:10 +0200

    Starting on a physics based player controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float movementSpeed = 2f;
+        private Rigidbody2D _rigidbody2D;
+        private Vector2 _movementDirection;
+        private void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            _movementDirection = new Vector2(Input.GetAxis("Horizontal"), -_rigidbody2D.mass);
+        }
+
+        private void FixedUpdate()
+        {
+            _rigidbody2D.velocity = _movementDirection * movementSpeed;
+        }
+    }
+}
