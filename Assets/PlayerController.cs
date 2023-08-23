ce6787543987712a5338476daea528593af527db
Carl Gulliksson <gulliksson.carl@gmail.com>
2023-08-23 11:59:20 +0200

    Basic running/jumping now working

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,8 +6,12 @@ namespace DefaultNamespace
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private float movementSpeed = 2f;
+        [SerializeField] private float jumpSpeed = 2f;
+        public LayerMask groundMask;
         private Rigidbody2D _rigidbody2D;
-        private Vector2 _movementDirection;
+        private Vector2 _move;
+        private bool jump = false;
+        
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -15,12 +19,25 @@ namespace DefaultNamespace
 
         private void Update()
         {
-            _movementDirection = new Vector2(Input.GetAxis("Horizontal"), -_rigidbody2D.mass);
+            _move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            if (_rigidbody2D.IsTouchingLayers(groundMask))
+            {
+                if (Input.GetButtonDown("Jump"))
+                {
+                    jump = true;
+                    Debug.Log("Jump: " + jump);
+                }
+            }
         }
 
         private void FixedUpdate()
         {
-            _rigidbody2D.velocity = _movementDirection * movementSpeed;
+            _rigidbody2D.velocity = new Vector2(_move.x * movementSpeed, _rigidbody2D.velocity.y);
+            if (jump)
+            {
+                jump = false;
+                _rigidbody2D.AddForce(new Vector2(0, jumpSpeed));
+            }
         }
     }
 }
